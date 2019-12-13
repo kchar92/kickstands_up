d7d9abb3ef826ae2877772c098f583287ddee3bc
Keelana Char <keelanachar@gmail.com>
2019-12-12 22:48:55 -0600

    create rides using seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# ride = Ride.new(name: "Two Wheel Tuesday", date_time: DateTime.parse("01/07/20 05:00PM").strftime("%m-%e-%y %H:%M"), starting_point: "630 N. Kingsley Dr. Los Angeles, CA 90004", starting_point_lat: 34.082590, starting_point_long: -118.303240, end_point: "1000 Vin Scully Ave, Los Angeles, CA 90012", end_point_lat: 34.072578, end_point_long: -118.240288, ride_distance: 7.5, bike_type: "sport")
+# ride.save!
+# ride = Ride.new(name: "Canyon Run", date_time: DateTime.parse("03/05/20 02:00PM").strftime("%m-%e-%y %H:%M"), starting_point: "630 N. Kingsley Dr. Los Angeles, CA 90004", starting_point_lat: 34.082590, starting_point_long: -118.303240, end_point: "8401 Mulholland Dr, Studio City, CA 91604", end_point_lat: 34.122580, end_point_long: -118.387090, ride_distance: 8.7, bike_type: "sport")
+# ride.save!
