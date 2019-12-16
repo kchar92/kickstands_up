2d95998a043b4d93e3067b6a6031b123ddd4aff5
Keelana Char <keelanachar@gmail.com>
2019-12-15 18:30:18 -0600

    create index and show actions for rides in controller

--- app/controllers/api/rides_controller.rb
+++ app/controllers/api/rides_controller.rb
@@ -1,2 +1,10 @@
 class Api::RidesController < ApplicationController
+  def index
+    @rides = Ride.all
+    render 'index.json.jb'
+  end
+  def show
+    @ride = Ride.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
 end
