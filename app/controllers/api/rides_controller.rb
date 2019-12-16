047d742dfecc8c48f2c174254121dd4b87bc10b3
Keelana Char <keelanachar@gmail.com>
2019-12-15 19:21:58 -0600

    create ride action

--- app/controllers/api/rides_controller.rb
+++ app/controllers/api/rides_controller.rb
@@ -3,6 +3,19 @@ class Api::RidesController < ApplicationController
     @rides = Ride.all
     render 'index.json.jb'
   end
+  def create
+    # use geocoder to get lat and long from starting_point and end_point??
+    @ride = Ride.new(
+      name: params[:name],
+      date_time: params[:date_time],
+      starting_point: params[:starting_point],
+      end_point: params[:end_point],
+      ride_distance: params[:ride_distance],
+      bike_type: params[:bike_type]
+    )
+    # @ride.save
+    render 'show.json.jb'
+  end
   def show
     @ride = Ride.find_by(id: params[:id])
     render 'show.json.jb'
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/rides" => "rides#index"
+    post "/rides" => "rides#create"
     get "/rides/:id" => "rides#show"
   end
 end
