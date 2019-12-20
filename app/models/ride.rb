f0b994101065ef2aad48e0397e0cdca8a9cd9294
Keelana Char <keelanachar@gmail.com>
2019-12-19 19:37:34 -0600

    created friendly_date_time

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -4,4 +4,8 @@ class Ride < ApplicationRecord
   has_many :ride_stations
   has_many :stations, through: :ride_stations
   has_many :comments
+
+  def friendly_date_time
+    date_time.strftime("%b %e, %l:%M %p")
+  end
 end
