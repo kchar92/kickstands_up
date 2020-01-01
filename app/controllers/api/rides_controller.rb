26b77abe05fcc8a37617800871e97ccd062784ad
Keelana Char <keelanachar@gmail.com>
2019-12-31 20:14:40 -0600

    add distance between, it works but not really

--- app/controllers/api/rides_controller.rb
+++ app/controllers/api/rides_controller.rb
@@ -18,6 +18,9 @@
     end_latitude = end_coordinates[0]
     end_longitude = end_coordinates[1]
 
+    # calculate distance between
+    distance = Geocoder::Calculations.distance_between(start_coordinates, end_coordinates)
+
     @ride = Ride.new(
       name: params[:name],
       date_time: params[:date_time],
@@ -27,14 +30,14 @@
       end_point: params[:end_point],
       end_point_lat: end_latitude,
       end_point_long: end_longitude,
-      ride_distance: params[:ride_distance],
+      ride_distance: distance,
       bike_type: params[:bike_type]
     )
-    if @ride.save
+    # if @ride.save
       render 'show.json.jb'
-    else
-      render json: { errors: @ride.errors.full_messages }, status: :unprocessable_entity
-    end
+    # else
+    #   render json: { errors: @ride.errors.full_messages }, status: :unprocessable_entity
+    # end
   end
   def show
     @ride = Ride.find_by(id: params[:id])
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -4,7 +4,7 @@ class Ride < ApplicationRecord
   has_many :ride_stations
   has_many :stations, through: :ride_stations
   has_many :comments
-
+ 
   def friendly_date_time
     date_time.strftime("%A, %d %b %Y %l:%M %p")
   end
