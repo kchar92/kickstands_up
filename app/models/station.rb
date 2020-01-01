2c4d2ac579f1d3de8635a7a44c51fb7ef64dd55f
Keelana Char <keelanachar@gmail.com>
2019-12-31 21:31:31 -0600

    add geocoder lat/long for stations

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,4 +1,16 @@
 class Station < ApplicationRecord
   has_many :ride_stations
   has_many :rides, through: :ride_stations
+  geocoded_by :address
+  after_validation :geocode
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode
+
+  def address
+    [street_address, city, state].compact.join(', ')
+  end
+
+  def coordinates
+    Geocoder.coordinates(address)
+  end
 end
